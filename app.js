4eea417e526798fd16e8a93927ef55da29074788
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-10-08 22:22:32 +0530

    another way add better that previous

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const bodyParser = require("body-parser");
 const exhbs = require("express-handlebars");
 const path = require("path");
 const pdfDoc = require("pdfkit");
+const pdf = require("pdf-creator-node");
 const fs = require("fs");
 
+var html = fs.readFileSync("template.html", "utf8");
+
+var options = {
+  format: "A3",
+  orientation: "portrait",
+  border: "10mm",
+  header: {
+    height: "45mm",
+    contents: '<div style="text-align: center;">Report</div>',
+  },
+  footer: {
+    height: "28mm",
+    contents: {
+      first: "Cover page",
+      2: "Second page", // Any page number is working. 1-based index
+      default:
+        '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+      last: "Last Page",
+    },
+  },
+};
 const doc = new pdfDoc();
 
 const app = express();
@@ -22,10 +44,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-  doc.pipe(fs.createWriteStream("repot.pdf"));
-  doc.fontSize(25).text(req.body.content);
-  doc.end();
-  res.render("contact", { msg: "PDF has been created..." });
+  // doc.pipe(fs.createWriteStream("repot.pdf"));
+  // doc.fontSize(25).text(req.body.content);
+  // doc.end();
+  var document = {
+    html: html,
+    data: {
+      desc: req.body.content,
+    },
+    path: "./output.pdf",
+  };
+  pdf
+    .create(document, options)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  // res.render("contact", { msg: "PDF has been created..." });
 });
 
 app.listen(3000, () => {
